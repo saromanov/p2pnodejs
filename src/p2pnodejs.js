e4d399e26703cc6b0a55ca0d7de9902cbb53a4d4
Sergey Romanov <xxsmotur@gmail.com>
2015-10-06 21:59:30 +0500

    Update

--- src/p2pnodejs.js
+++ src/p2pnodejs.js
@@ -3,11 +3,12 @@ import events from 'events';
 
 
 export class P2PNodejs {
-    construct(name){
+    constructor(name){
         this.name = name;
 
         //list of known addresses
-        this.known = [];
+        this.known = new Map();
+        this.blocked = new Map();
 
         this.socket = undefined;
     }
@@ -19,7 +20,7 @@ export class P2PNodejs {
             let buffer = new Buffer(msg, "utf-8");
             let out = new Buffer(msg.length + 24);
             buffer.copy(out, 24);
-            response.write(msg, null);
+            response.write(msg);
 
         }).on("connect", () => {
             console.log("Connected");
@@ -27,6 +28,7 @@ export class P2PNodejs {
           console.log(data);  
         });
         this.socket = socket;
+        console.log(this.socket);
     }
 
     receive() {
@@ -37,15 +39,32 @@ export class P2PNodejs {
 
     send(addr, data) {
 
-        let buffer = new Buffer(data);
-        this.socket.write(addr, data);
+    }
+
+    //Add address for blocked list
+    addBlocked(items) {
+        for(let item in items) {
+            this.blocked.set(item,1);
+        }
     }
 
     startServer(host='127.0.0.1', port=12345) {
         let server = net.createServer((socket) => {
-            console.log(socket.remoteAddress, socket.remotePort);
-            socket.write("Listen connections: ");
+            let addr = `${socket.remoteAddress}:${socket.remotePort}`;
+            if(this.blocked.hasOwnProperty(addr)) {
+                socket.write("This address in the black list");
+                socket.end();
+            } else {
+                this.known.set(addr, []);
+                socket.write("Listen connections: ");
+            }
+
+            socket.on('data', (data) => {
+                console.log("This is data: ", data);
+            });
+
             socket.end();
+
         });
 
         server.listen(port, host); 
